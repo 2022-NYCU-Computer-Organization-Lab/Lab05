3ed46a997da1eb3a7a67cbcce0db2f5e140a9fd8
yoonaiu <free.cs09@nycu.edu.tw>
2022-05-18 20:12:33 +0800

    finish

--- Lab5Code/Hazard_detection.v
+++ Lab5Code/Hazard_detection.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 module Hazard_detection(
-    input [4:0] IFID_regRs,
-    input [4:0] IFID_regRt,
+    input [4:0] IFID_regRs,     // rs1
+    input [4:0] IFID_regRt,     // rs2
     input [4:0] IDEXE_regRd,
     input IDEXE_memRead,
     output reg PC_write,
@@ -9,5 +9,24 @@ module Hazard_detection(
     output reg control_output_select
 );
 /* Write your code HERE */
+
+if (IDEXE_memRead == 1'b1) begin
+    if ( IFID_regRs == IDEXE_regRd ) begin
+        PC_write    = 1'b0;
+        IFID_write  = 1'b0;
+        control_output_select = 1'b1;
+    end 
+    else if ( IFID_regRt == IDEXE_regRd ) begin
+        PC_write    = 1'b0;
+        IFID_write  = 1'b0;
+        control_output_select = 1'b1;
+    end
+end
+else begin
+    PC_write    = 1'b1;
+    IFID_write  = 1'b1;
+    control_output_select = 1'b0;
+end
+
 endmodule
 
