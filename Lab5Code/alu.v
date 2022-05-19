f9b7f69692148f858e51e0345ee0cb7eb31d122e
yoonaiu <free.cs09@nycu.edu.tw>
2022-05-20 00:38:27 +0800

    feature: finish implementation of alu

--- Lab5Code/alu.v
+++ Lab5Code/alu.v
@@ -10,4 +10,27 @@ module alu(
 );
 
 /* Write your code HERE */
+always @(*) begin
+    case( ALU_control[3:0] )
+        4'b0000:    // and
+            result = src1 & src2;
+        4'b0001:    // or
+            result = src1 | src2;
+        4'b0010:    // add
+            result = src1 + src2;
+        4'b0110:    // sub
+            result = src1 - src2;
+        4'b0011:    // xor
+            result = src1 ^ src2;
+        4'b0111:    // slt
+            result = (src1 < src2)? {{31{1'b0}}, 1'b1} : {32'b0};
+        4'b0100:    // slti
+            result = (src1 < src2)? {{31{1'b0}}, 1'b1} : {32'b0};
+        4'b0101:    // slli
+            result = src1 <<< src2;
+    endcase	
+end
+
+assign Zero = ~(|result);
+
 endmodule
