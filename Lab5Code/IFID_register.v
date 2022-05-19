ffdbe34034f24aabcd5c5774a48ad1e8f1e8d013
yoonaiu <free.cs09@nycu.edu.tw>
2022-05-19 21:28:51 +0800

    feature: finish implementation of IFID_register

--- Lab5Code/IFID_register.v
+++ Lab5Code/IFID_register.v
@@ -3,6 +3,7 @@ module IFID_register (
     input clk_i,
     input rst_i,
     input flush,
+    input write,
     input [31:0] address_i,
     input [31:0] instr_i,
     input [31:0] pc_add4_i,
@@ -11,17 +12,31 @@ module IFID_register (
     output reg [31:0] pc_add4_o
 );
 /* Write your code HERE */
-// 感覺 flush 對 IFID_register 內部沒什麼關西啊，主要是外面的 control 要全 0
+/*
+    if clk come
+        1. if rst == 0: all reset to 0
+        2. if flush == 1: all flush to 0
+        3. write = 1: load
+        else: remain
+*/
 
 always @(posedge clk_i) begin
-    if(~rst_i)
+    if (~rst_i) begin
         address_o   <= 0;
         instr_o     <= 0;
         pc_add4_o   <= 0;
-    else
+    end
+    else if (flush == 1'b1) begin
+        address_o   <= 0;
+        instr_o     <= 0;
+        pc_add4_o   <= 0;
+    end
+    else if ( write == 1'b1 ) begin
         address_o   <= address_i;
         instr_o     <= instr_i;
         pc_add4_o   <= pc_add4_i;
+    end
+    // else remain
 end
 
 endmodule
